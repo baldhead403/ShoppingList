015f9f1f2cc39db785461c93da71d55b4c4be8e0
COR Students <corstudents@Matts-MBP-97.localdomain>
2019-03-04 09:54:58 -0500

    enter

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
+const db = require('../../config/keys').mongoURI
 
+mongoose.connect(db ,  { useNewUrlParser: true })
+.then(() => console.log('MongoDb Connected...'))
+.catch(err => console.log(err))
 // Items Model
 const Item = require('../../models/Items')
 
--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const items = require('./routes/api/items')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //DB Config
 const db = require('./config/keys').mongoURI
@@ -16,6 +21,6 @@ mongoose.connect(db ,  { useNewUrlParser: true })
 
 app.use('/api/items', items)
 
-const port = process.env.Port || 3001
+const port = process.env.Port || 3002
 
 app.listen(port, () => console.log('Server started on port ' + port))
