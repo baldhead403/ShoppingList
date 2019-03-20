d4855999220afd3276d962c89bf204273bb4d9f0
COR Students <corstudents@Matts-MBP-97.attlocal.net>
2019-03-20 15:55:53 -0400

    enter

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb://shopper1:sh0pnow@ds133875.mlab.com:33875/shoppinglist'
+    mongoURI:('mongodb://shopper1:sh0pnow@ds133875.mlab.com:33875/shoppinglist')
 }
--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const items = require('./routes/api/items')
-const MongoClient = require('mongodb').MongoClient
+const path = require('path')
 const app = express()
 
 app.use(bodyParser.json())
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //DB Config
 const db = require('./config/keys').mongoURI
@@ -23,3 +23,15 @@ mongoose
 
 app.use('/api/items', items)
 
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.Port || 5001
+
+// app.listen(port, () => console.log('Server started on port ' + port))
